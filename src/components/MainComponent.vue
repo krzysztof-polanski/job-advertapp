5eb50c19f97bf0bbf6ed34716193e3f51072b326
krzysztof-polanski <59507139+krzysztof-polanski@users.noreply.github.com>
2021-04-10 17:58:07 +0200

    Rebuild Todo to become Job Advertisement

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h2 class="closed-box">Do zrobienia: {{ pending }}</h2>
+        <h2 class="closed-box" @click="colorBG">Zrobione: {{ closed }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['closed', 'pending'],
+    methods: {
+        colorBG() {
+            const box = this.closed
+            this.$emit('fuckThisShit', box)
+        }
+    }
+}
+</script>
+
+<style>
+    .closed-box {
+        width: 100px;
+        height: 100px;
+        padding: 20px;
+        margin: 20px;
+        border: 2px solid lightseagreen;
+        text-align: center;
+        vertical-align: middle;
+    }
+</style>
